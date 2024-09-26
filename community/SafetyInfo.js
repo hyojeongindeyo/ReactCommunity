afff8c83b3c046f3ff34d58acbf95c4a4ca8d4ad
hyojeongindeyo <ghdgywjd510@naver.com>
2024-09-26 23:25:04 +0900

    카드뉴스 업로드

--- community/SafetyInfo.js
+++ community/SafetyInfo.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, ScrollView, Dimensions, Modal, TextInput, TouchableWithoutFeedback } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView, Dimensions, Modal, TextInput, TouchableWithoutFeedback, ActivityIndicator, Image } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import BottomTabBar from '../BottomTabBar';
+import axios from 'axios'; // Axios 임포트
+import config from '../config';
 
 const { width: SCREEN_WIDTH } = Dimensions.get('window');
 
@@ -13,23 +15,13 @@ const SafetyInfo = ({ navigation, route }) => {
   const [selectedInfo, setSelectedInfo] = useState(null);
   const [searchQuery, setSearchQuery] = useState('');
   const [searchHistory, setSearchHistory] = useState([]);
+  const [safetyInfos, setSafetyInfos] = useState([]); // 백엔드에서 불러온 데이터 저장
+  const [loading, setLoading] = useState(true); // 로딩 상태 관리
+  const [images, setImages] = useState([]); // 선택된 정보의 이미지 목록 저장
+  const [currentImageIndex, setCurrentImageIndex] = useState(0); // 현재 보고 있는 이미지 인덱스
+
 
   const categories = ['전체', '자연', '사회', '생활'];
-
-  const safetyInfos = [
-    { id: 1, title: '화재 시\n행동요령', date: '2024.07.01', category: '자연', banner: '화재 시 행동요령' },
-    { id: 2, title: '태풍 시\n행동요령', date: '2024.07.02', category: '자연', banner: '태풍 시 대피요령' },
-    { id: 3, title: '교통사고', date: '2024.07.03', category: '사회', banner: '교통사고 예방수칙' },
-    { id: 4, title: '심폐소생술', date: '2024.07.04', category: '생활', banner: '심폐소생술 방법' },
-    { id: 5, title: '침수 시\n행동요령', date: '2024.07.05', category: '자연', banner: '침수 시 예방수칙' },
-    { id: 6, title: '뺑소니', date: '2024.07.06', category: '사회', banner: '뺑소니 대처법' },
-    { id: 7, title: '응급처치', date: '2024.07.07', category: '생활', banner: '응급처치 방법' },
-    { id: 8, title: '폭우 시\n예방수칙', date: '2024.07.01', category: '자연', banner: '폭우 시 예방수칙' },
-    { id: 9, title: '산불 예방\n수칙', date: '2024.07.08', category: '자연', banner: '산불 예방수칙' },
-    { id: 10, title: '오물풍선 발견 시\n행동요령', date: '2024.07.10', category: '생활', banner: '오물풍선 발견 시 행동요령' },
-  ];
-
-  const filteredInfos = selectedCategory === '전체' ? safetyInfos : safetyInfos.filter(info => info.category === selectedCategory);
 
   useEffect(() => {
     if (filter) {
@@ -37,16 +29,45 @@ const SafetyInfo = ({ navigation, route }) => {
     }
   }, [filter]);
 
-  const handleInfoPress = (info) => {
+  // 백엔드에서 데이터 가져오기
+  useEffect(() => {
+    const fetchSafetyInfos = async () => {
+      try {
+        const response = await axios.get(`${config.apiUrl}/safetyInfos`); // API URL로 변경
+        setSafetyInfos(response.data); // API로부터 받은 데이터를 저장
+        setLoading(false); // 데이터 로딩 완료
+      } catch (error) {
+        console.error('Error fetching safety info:', error);
+        setLoading(false); // 에러 발생 시에도 로딩 상태 해제
+      }
+    };
+
+    fetchSafetyInfos();
+  }, []);
+
+  const filteredInfos = selectedCategory === '전체' ? safetyInfos : safetyInfos.filter(info => info.category === selectedCategory);
+
+  const handleInfoPress = async (info) => {
     setSelectedInfo(info);
     setInfoModalVisible(true);
+    await fetchImages(info.id); // 선택된 정보의 이미지 목록 가져오기
+
+  };
+
+  const fetchImages = async (infoId) => {
+    try {
+      const response = await axios.get(`${config.apiUrl}/cardnews/${infoId}`); // 해당 정보 ID로 이미지 가져오기
+      setImages(response.data); // 가져온 이미지 목록 설정
+    } catch (error) {
+      console.error('Error fetching images:', error);
+      setImages([]); // 에러 발생 시 이미지 목록 초기화
+    }
   };
 
   const handleBannerPress = (banner) => {
     const info = safetyInfos.find(info => info.banner === banner);
     if (info) {
-      setSelectedInfo(info);
-      setInfoModalVisible(true);
+      handleInfoPress(info);
     }
   };
 
@@ -73,57 +94,67 @@ const SafetyInfo = ({ navigation, route }) => {
         </TouchableOpacity>
       </View>
 
-      <View style={styles.bannerContainer}>
-        <ScrollView horizontal pagingEnabled showsHorizontalScrollIndicator={false} style={styles.banner}>
-          <TouchableOpacity style={[styles.bannerItem, styles.firstBanner]} onPress={() => handleBannerPress('폭우 시 예방수칙')}>
-            <Text style={styles.bannerSubtitle}>여름철 빈번하게 발생하는</Text>
-            <Text style={styles.bannerText}>폭우 시 예방수칙</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={[styles.bannerItem, styles.secondBanner]} onPress={() => handleBannerPress('화재 시 행동요령')}>
-            <Text style={styles.bannerSubtitle}>북한 오물풍선</Text>
-            <Text style={styles.bannerText}>발견 시 행동요령</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={[styles.bannerItem, styles.thirdBanner]} onPress={() => handleBannerPress('산불 예방수칙')}>
-            <Text style={styles.bannerSubtitle}>봄철 산불 예방</Text>
-            <Text style={styles.bannerText}>산불 예방수칙</Text>
-          </TouchableOpacity>
-        </ScrollView>
-      </View>
-
-      <View style={styles.categoryContainer}>
-        {categories.map((category) => (
-          <TouchableOpacity
-            key={category}
-            onPress={() => setSelectedCategory(category)}
-            style={styles.categoryButton}
-          >
-            <Text style={[styles.categoryText, selectedCategory === category && styles.selectedCategoryText]}>
-              {category}
-            </Text>
-          </TouchableOpacity>
-        ))}
-      </View>
-
-      <ScrollView contentContainerStyle={styles.infoContainer}>
-        <View style={styles.infoRow}>
-          {filteredInfos.map((info) => (
-            <TouchableOpacity key={info.id} onPress={() => handleInfoPress(info)} style={styles.infoCardContainer}>
-              <View style={styles.infoCard}>
-                <Text style={styles.infoTitle}>{info.title}</Text>
-              </View>
-              <View style={styles.infoFooter}>
-                <Text style={styles.infoDate}>{info.date}</Text>
-                <View style={styles.categoryBadge}>
-                  <Text style={styles.categoryBadgeText}>{info.category}</Text>
-                </View>
-              </View>
-            </TouchableOpacity>
-          ))}
+      {/* 로딩 상태일 때 표시 */}
+      {loading ? (
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator size="large" color="#0000ff" />
         </View>
-      </ScrollView>
+      ) : (
+        <>
+          <View style={styles.bannerContainer}>
+            <ScrollView horizontal pagingEnabled showsHorizontalScrollIndicator={false} style={styles.banner}>
+              <TouchableOpacity style={[styles.bannerItem, styles.firstBanner]} onPress={() => handleBannerPress('폭우 시 예방수칙')}>
+                <Text style={styles.bannerSubtitle}>여름철 빈번하게 발생하는</Text>
+                <Text style={styles.bannerText}>폭우 시 예방수칙</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={[styles.bannerItem, styles.secondBanner]} onPress={() => handleBannerPress('화재 시 행동요령')}>
+                <Text style={styles.bannerSubtitle}>북한 오물풍선</Text>
+                <Text style={styles.bannerText}>발견 시 행동요령</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={[styles.bannerItem, styles.thirdBanner]} onPress={() => handleBannerPress('산불 예방수칙')}>
+                <Text style={styles.bannerSubtitle}>봄철 산불 예방</Text>
+                <Text style={styles.bannerText}>산불 예방수칙</Text>
+              </TouchableOpacity>
+            </ScrollView>
+          </View>
+
+          <View style={styles.categoryContainer}>
+            {categories.map((category) => (
+              <TouchableOpacity
+                key={category}
+                onPress={() => setSelectedCategory(category)}
+                style={styles.categoryButton}
+              >
+                <Text style={[styles.categoryText, selectedCategory === category && styles.selectedCategoryText]}>
+                  {category}
+                </Text>
+              </TouchableOpacity>
+            ))}
+          </View>
+
+          <ScrollView contentContainerStyle={styles.infoContainer}>
+            <View style={styles.infoRow}>
+              {filteredInfos.map((info) => (
+                <TouchableOpacity key={info.id} onPress={() => handleInfoPress(info)} style={styles.infoCardContainer}>
+                  <View style={styles.infoCard}>
+                    <Text style={styles.infoTitle}>{info.title}</Text>
+                  </View>
+                  <View style={styles.infoFooter}>
+                    <Text style={styles.infoDate}>{info.date}</Text>
+                    <View style={styles.categoryBadge}>
+                      <Text style={styles.categoryBadgeText}>{info.category}</Text>
+                    </View>
+                  </View>
+                </TouchableOpacity>
+              ))}
+            </View>
+          </ScrollView>
+        </>
+      )}
 
       <BottomTabBar navigation={navigation} />
 
+      {/* Info Modal */}
       <Modal
         animationType="slide"
         transparent={true}
@@ -132,17 +163,45 @@ const SafetyInfo = ({ navigation, route }) => {
       >
         <View style={styles.modalOverlay}>
           <View style={styles.modalContent}>
-            <Text style={styles.modalTitle}>{selectedInfo?.banner}</Text>
+            <Text style={styles.modalTitle}>{selectedInfo?.title}</Text>
             <TouchableOpacity style={styles.modalCloseButton} onPress={() => setInfoModalVisible(false)}>
               <Text style={styles.modalCloseText}>X</Text>
             </TouchableOpacity>
-            <View style={styles.grayRectangle}>
-              <Text style={styles.rectangleX}>X</Text>
+            {/* <View style={styles.grayRectangle}> */}
+              {images.length > 0 ? (
+                <>
+                  <Image
+                    key={currentImageIndex}
+                    source={{ uri: images[currentImageIndex] }} // 현재 이미지 URL 사용
+                    style={styles.cardImage}
+                    resizeMode="contain" // 비율을 유지하며 컨테이너 안에 맞춤
+                    onError={(error) => console.error('Image loading error:', error.nativeEvent.error)}
+                  />
+                  <View style={styles.imageNavigation}>
+                    <TouchableOpacity
+                      onPress={() => setCurrentImageIndex((prevIndex) => Math.max(prevIndex - 1, 0))}
+                      disabled={currentImageIndex === 0} // 첫 번째 이미지일 경우 비활성화
+                    >
+                      <Text style={styles.navigationText}>이전</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                      onPress={() => setCurrentImageIndex((prevIndex) => Math.min(prevIndex + 1, images.length - 1))}
+                      disabled={currentImageIndex === images.length - 1} // 마지막 이미지일 경우 비활성화
+                    >
+                      <Text style={styles.navigationText}>다음</Text>
+                    </TouchableOpacity>
+                  </View>
+                </>
+              ) : (
+                <Text>No images available.</Text>
+              )}
             </View>
           </View>
-        </View>
+        {/* </View> */}
       </Modal>
 
+
+      {/* Search Modal */}
       <Modal
         animationType="slide"
         transparent={true}
@@ -160,7 +219,7 @@ const SafetyInfo = ({ navigation, route }) => {
                   <TextInput
                     style={styles.searchInput}
                     placeholder="검색어를 입력하세요"
-                    placeholderTextColor="#888888" 
+                    placeholderTextColor="#888888"
                     value={searchQuery}
                     onChangeText={setSearchQuery}
                     autoFocus
@@ -334,9 +393,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   modalTitle: {
-    fontSize: 18,
+    fontSize: 20,
     fontWeight: 'bold',
-    marginBottom: 10,
+    // marginBottom: 5,
   },
   modalCloseButton: {
     position: 'absolute',
@@ -347,14 +406,9 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
   },
-  grayRectangle: {
-    backgroundColor: '#e0e0e0',
-    width: '90%',
-    height: 320,
-    marginTop: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
+  // grayRectangle: {
+  //   width: '100%',
+  // },
   rectangleX: {
     color: 'black',
     fontSize: 250,
@@ -425,6 +479,24 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: '#ff0000',
   },
+  cardImage: {
+    width: '100%',
+    height: 300, // 원하는 높이로 조정
+    marginVertical: 5,
+  },
+  imageNavigation: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    marginTop: 10,
+  },
+  navigationText: {
+    fontSize: 18,
+    color: '#007BFF', // 버튼 색상
+    fontWeight: 'bold',
+  },
+
 });
+
 
 export default SafetyInfo;
